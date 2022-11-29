d27a9520dccda56d975db14ce31eb0156b0f47c3
Pedro Pereira <pmatiucci@gmail.com>
2022-11-29 16:18:10 -0300

    Exercicio2 completo

---
+++ Exercicio2/IndiceRemissivo.cs
@@ -0,0 +1,71 @@
+﻿namespace Exercicio2
+{
+    internal class IndiceRemissivo
+    {
+        Dictionary<string, int> DicContador = new Dictionary<string, int>();
+        Dictionary<string, List<int>> DicLinha = new Dictionary<string, List<int>>();
+        char[] chars = { ' ', '.', ',', ';', '<', '>', ':', '\\', '/', '|', '~', '^', '´', '`', '[', ']', '{', '}', '‘', '“', '!', '@', '#', '$', '%', '&', '%', '&', '*', '(', ')', '_', '+', '=' };
+        public string PathTXT { get; set; }
+        public string PathIgnore { get; set; }
+
+        public IndiceRemissivo(string PathTXT, string PathIgnore = " ")
+        {
+            this.PathTXT = PathTXT;
+            this.PathIgnore = PathIgnore;
+            ContaPalavras();
+        }
+
+        public void ContaPalavras()
+        {
+            try
+            {
+                string[] lines = File.ReadAllLines(PathTXT);
+                string ignore = " ";
+                if (PathIgnore != " ")
+                {
+                    ignore = File.ReadAllText(PathIgnore);
+                }
+                int i = 0;
+                foreach (string line in lines)
+                {
+                    string[] words = line.Split(chars);
+                    foreach (string word in words)
+                    {
+                        string w = word.Trim().ToLower();
+                        if (!DicContador.ContainsKey(w))
+                        {
+                            if (!ignore.Contains(w))
+                            {
+                                DicContador.Add(w, 1);
+                                List<int> listaTemp = new List<int> { i };
+                                DicLinha.Add(w, listaTemp);
+                            }
+                        }
+                        else
+                        {
+                            DicContador[w] += 1;
+                            if (!DicLinha[w].Contains(i))
+                            {
+                                DicLinha[w].Add(i);
+                            }
+                        }
+                    }
+                    i++;
+                }
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void Imprime()
+        {
+            foreach (KeyValuePair<string, int> word in DicContador.OrderBy(key => key.Key))
+            {
+                string numeroLinha = string.Join(", ", DicLinha[word.Key]);
+                Console.WriteLine("{0} ({1}) {2}", word.Key.ToUpper(), word.Value, numeroLinha);
+            }
+
+        }
+    }
+}
---
+++ Exercicio2/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Exercicio2;
+
+string pathTXT = "C:\\Users\\Carla\\Desktop\\Unidade 1 - Exercicios - Parte 3\\texto.txt";
+string ignore = "C:\\Users\\Carla\\Desktop\\Unidade 1 - Exercicios - Parte 3\\ignore.txt";
+IndiceRemissivo indiceRemissivo = new IndiceRemissivo(pathTXT, ignore);
+//IndiceRemissivo indiceRemissivo2 = new IndiceRemissivo(pathTXT);
+indiceRemissivo.Imprime();
